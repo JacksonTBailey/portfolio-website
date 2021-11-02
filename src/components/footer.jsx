94d831478a88d5a24f1712cdf118c9ff6db6d9ad
JacksonTBailey <bailey95jackson@gmail.com>
2021-11-02 17:02:40 -0400

    Created footer and scss for footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,17 +6,28 @@ export default function Footer() {
         {name: "github", url: "https://github.com/Kazumadesu67", class: "fab fa-github"},
         {name: "email", url: "mailto: bailey95jackson@gmail.com", class: "far fa-envelope"}
     ]
+    
+    const featuredQuote = [
+        {author: "Vincent van Gogh", quote: "Normality is a paved road: It’s comfortable to walk, but no flowers grow on it."},
+        {author: "Leonardo da Vinci", quote: "Learning never exhausts the mind."},
+        {author: "Katsumoto — The Last Samurai", quote: "The perfect blossom is a rare thing. You could spend your life looking for one, and it would not be a wasted life."},
+        {author: "Obi-Wan Kenobi", quote: "Hello there."}
+    ]
 
     const mappedSocials = socialMedia.map((socialItem)=>{return <div key={socialItem.name} className={`social ${socialItem.name}`}>
             <a href={socialItem.url} target="_blank" rel="noreferrer">
-                <i className={`${socialItem.class} fa-lg`} aria-hidden="true"></i>
+                <i className={`${socialItem.class} footer-social fa-lg`} aria-hidden="true"></i>
             </a>
         </div>})
+    
+    const randomQuote= Math.floor(Math.random()*featuredQuote.length)
+
+
     return (
-        <footer>
+        <footer className="regular-footer">
             <div className="footer-quote">
-                <p className="quote-text">Normality is a paved road: It’s comfortable to walk, but no flowers grow on it.</p>
-                <p className="quote-author">Vincent van Gogh</p>
+                <p className="quote-text">{featuredQuote[randomQuote].quote}</p>
+                <p className="quote-author">{featuredQuote[randomQuote].author}</p>
             </div>
             {mappedSocials}
         </footer>
