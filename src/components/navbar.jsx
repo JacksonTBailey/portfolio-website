9b1f0baa1f1b27b93fe91801f488bb5afd5d3b51
JacksonTBailey <bailey95jackson@gmail.com>
2021-11-18 16:47:33 -0500

    Dev: Finished navbar functionality and started basic styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,53 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <header className="primary-header flex">
+export function Navbar() {
+    return (
+        <header className="primary-header flex">
             
-            <img src="./images/logo.svg" alt="Logo" className="logo" />
-            
-            <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false" onLoad={hamburgerToggle}>
-                <span className="sr-only">Menu</span>
-            </button>
-            
-            <nav>
-                <ul id= "primary-navigation" className="primary-navigation flex" data-visible="false">
-                    <li className="active">
-                        <a href="home.jsx">
-                            <span aria-hidden="true">00</span>Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="projects.jsx">
-                            <span aria-hidden="true">01</span>Projects
-                        </a>
-                    </li>
-                    <li>
-                        <a href="about.jsx">
-                            <span aria-hidden="true">02</span>About
-                        </a>
-                    </li>
-                    <li>
-                        <a href="contact.jsx">
-                            <span aria-hidden="true">03</span>Contact
-                        </a>
-                    </li>
+        <img src="./images/jb-logo-2.svg" alt="Logo" className="logo" />
+        
+        <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false" onClick={hamburgerToggle}>
+            <span className="sr-only">Menu</span>
+        </button>
+        
+        <nav>
+            <ul id= "primary-navigation" className="primary-navigation flex" data-visible="false">
+                <li className="active">
+                    <a href="home.jsx">
+                        <span aria-hidden="true">01.</span>Home
+                    </a>
+                </li>
+                <li>
+                    <a href="projects.jsx">
+                        <span aria-hidden="true">02.</span>Projects
+                    </a>
+                </li>
+                <li>
+                    <a href="about.jsx">
+                        <span aria-hidden="true">03.</span>About
+                    </a>
+                </li>
+                <li>
+                    <a href="contact.jsx">
+                        <span aria-hidden="true">04.</span>Contact
+                    </a>
+                </li>
 
-                </ul>
-            </nav>
-        </header>
-        );
-    }
+            </ul>
+        </nav>
+    </header>
+    );
 }
-
-
     function hamburgerToggle(){
         const primaryNav = document.querySelector('.primary-navigation');
         const navToggle = document.querySelector('.mobile-nav-toggle');
-    
-        navToggle.addEventListener('click', ()=> {
-            const visibility = primaryNav.getAttribute('data-visible');
+        const visibility = primaryNav.getAttribute('data-visible');
             if (visibility==="true"){
                 primaryNav.setAttribute("data-visible", false);
                 navToggle.setAttribute('aria-expanded', false)
@@ -55,5 +49,4 @@ export default class Navbar extends Component {
                 primaryNav.setAttribute("data-visible", true); 
                 navToggle.setAttribute('aria-expanded', true);
             }
-        })
-    }
+        }
