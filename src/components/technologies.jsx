cdcfc3e12fbd8004ea40b7f7e6b6dbc1b3cdac0f
JacksonTBailey <bailey95jackson@gmail.com>
2022-01-21 12:03:20 -0500

    Added technologies section

---
+++ src/components/technologies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function technologies() {
+  return(
+      //https://www.youtube.com/watch?v=OPaLnMw2i_0
+      <Section className="tech" id="tech">
+          <h2 className='tech-header'>Technologies</h2>
+          <p>I've worked with a vast array of technologies </p>
+
+      </Section>
+  );
+}
