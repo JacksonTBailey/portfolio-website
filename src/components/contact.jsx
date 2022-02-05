7fdb02cd37ce1fe3870d02e1db885c2fcb36356f
Jackson Bailey <Bailey95jackson@gmail.com>
2022-02-04 22:49:53 -0500

    Started creating contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div>
-            
-        </div>
+        <form action="https://formsubmit.co/bailey95jackson@gmail.com" method='POST'>
+            <input type="hidden" name="_subject" value="New form submission!"></input>
+            <input type="hidden" name="_next" value="https://jacksonbailey.design/thankyou.jsx"></input>
+            <input type="text" name="_honey" style="display:none"></input>
+            <input type="text" name='name' placeholder='Name' required />
+            <input type="email" name='email' placeholder="Email Address" required />
+            <input type="text" name='message' placeholder='Write your message here' required />
+            <button type='submit'>Send</button>
+        </form>
     )
 }
