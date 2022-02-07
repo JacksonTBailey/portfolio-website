e7b3b77ee4280b5bc5b5bffb4db5e3c6073dc80f
JacksonTBailey <bailey95jackson@gmail.com>
2022-02-07 18:30:42 -0500

    Dev: Started creating my About Me section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            
-        </div>
+        <section className='about-section' id='about-me'>
+            <h2>About Me</h2>
+            <p>I'm a Frontend Web Devleoper who enjoys crafting exquisite and minimalist web applications with intuitive user interfaces.</p>
+        </section>
     )
 }
