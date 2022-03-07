efcda25af16e51c47a76be59180663795aa6e1c9
Jackson Bailey <jbailey@otodevelopment.com>
2022-03-07 14:03:03 -0500

    Dev: Mobile Optimization

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -32,33 +32,33 @@ export default function Projects() {
             title: "Adventures in Yolrein",
             number:1,
             description: `A semi-open-world, text adventure game I created using vanilla JavaScript. Unlike most text adventure games, Adventures in Yolrein allows you to explore the world and unlock additional content in previously visited areas based on choices you've made since the first time you've visited.`,
-            technologies: ["HTML", "CSS", "Flexbox", "JavaScript", "Atom", "GitHub"],
+            technologies: ["HTML", "CSS", "Flexbox", "JavaScript"],
             links: [
                 {name: "GitHub", url: "https://github.com/JacksonTBailey/Text_Adventure_Game", class: "fab fa-github"},
                 {name: "External Link", url: "https://jacksontbailey.github.io/Text_Adventure_Game/", class: "fas fa-external-link-alt"}
                 ],
             image: [
-                {src: recipes_r_us, alt: "Adventures in Yolrein"},
+                {src: text_adventure_game, alt: "Adventures in Yolrein"},
             ]
         },
         {
             title: "Recipes R' Us",
             number:2,
             description: `A cooking recipe website that runs off of an API created by Crescendo Collective. Besides this portfolio website, this was the second project I ever created using React.`,
-            technologies: ["HTML", "CSS", "SCSS", "Styled Components", "JavaScript", "React", "React Router", "GitHub"],
+            technologies: ["HTML", "CSS", "SCSS", "JavaScript", "React", "React Router"],
             links: [
                 {name: "GitHub", url: "https://github.com/JacksonTBailey/recipes-r-us", class: "fab fa-github"},
                 {name: "External Link", url: "https://recipes-r-us.netlify.app/recipes", class: "fas fa-external-link-alt"}
                 ],
             image: [
-                {src: text_adventure_game, alt: "Recipes R' Us"},
+                {src: recipes_r_us, alt: "Recipes R' Us"},
             ]
         },
         {
             title: "Weather Wherever",
             number:3,
             description: `Get the weather for any city throughout the world through my weather API website. Don't want to view it in Fahrenheit? No problem! View it in Celcius too!`,
-            technologies: ["HTML", "CSS", "Styled Components","JavaScript", "REST API","OpenWeatherMap API"],
+            technologies: ["HTML", "CSS","JavaScript", "APIs"],
             links: [
                 {name: "GitHub", url: "https://github.com/JacksonTBailey/WeatherApi", class: "fab fa-github"},
                 {name: "External Link", url: "https://weather-wherever.netlify.app/", class: "fas fa-external-link-alt"}
@@ -71,7 +71,7 @@ export default function Projects() {
             title: "Simon",
             number:4,
             description: `A remake of the classic game of simon. This was one of my first projects using JavaScript that I worked on, and still enjoy it to this day!`,
-            technologies: ["HTML", "CSS", "JavaScript", "Atom", "Git", "GitHub"],
+            technologies: ["HTML", "CSS", "JavaScript"],
             links: [
                 {name: "GitHub", url: "https://github.com/JacksonTBailey/Simon-Game", class: "fab fa-github"},
                 {name: "External Link", url: "https://crabby-curve.surge.sh/", class: "fas fa-external-link-alt"}
@@ -151,9 +151,11 @@ export default function Projects() {
                     <ul className="project-tech-used">{project.technologies.map(technology => {
                         return(<li key={technology}>{technology}</li>)})}
                     </ul>
-                    <button className='card-flip-button-back' onClick={() => handleClick(project.number)}>Less Info</button>                
-                    <div className="project-links-flip">{project.links.map(link =>{
-                        return(<a href={link.url} target="_blank" rel="noreferrer" key={link.class}><i className={link.class}></i></a>)})}
+                    <div className='all-buttons-back'>
+                        <button className='card-flip-button-back' onClick={() => handleClick(project.number)}>Less Info</button>                
+                        <div className="project-links-flip">{project.links.map(link =>{
+                            return(<a href={link.url} target="_blank" rel="noreferrer" key={link.class}><i className={link.class}></i></a>)})}
+                        </div>
                     </div> 
                 </section>
 
